5cfd3c319a96af62e5ae39521d164aaa59302126
Brian Rosner <brosner@gmail.com>
2008-05-30 16:16:28 +0000

    Added a pass through clean method for the formfield LocalizedDateTimeField.
    git-svn-id: https://django-timezones.googlecode.com/svn/trunk@4 86ebb30f-654e-0410-bc0d-7bf82786d749

--- timezones/forms.py
+++ timezones/forms.py
@@ -1,5 +1,7 @@
 
 from django import newforms as forms
 
-class LocalizedDateTimeField(forms.Field):
-    pass
+class LocalizedDateTimeField(forms.DateTimeField):
+    def clean(self, value):
+        value = super(LocalizedDateTimeField, self).clean(value)
+        return value
