659ea2bb24841b670804f95e20ab0ceaffc708e7
Brian Rosner <brosner@gmail.com>
2008-05-30 16:49:00 +0000

    Added a docstring explaining fields.LocalizedDateTimeField.
    git-svn-id: https://django-timezones.googlecode.com/svn/trunk@8 86ebb30f-654e-0410-bc0d-7bf82786d749

--- timezones/fields.py
+++ timezones/fields.py
@@ -4,6 +4,9 @@ from django.db import models
 from timezones import forms
 
 class LocalizedDateTimeField(models.DateTimeField):
+    """
+    A model field that provides automatic integration with a ModelForm.
+    """
     def formfield(self, **kwargs):
         defaults = {"form_class": forms.LocalizedDateTimeField}
         defaults.update(kwargs)
