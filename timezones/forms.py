64f1fef46bb404ddb53de8286a05d22bb2198122
Brian Rosner <brosner@gmail.com>
2008-05-30 16:48:04 +0000

    Made timezone argument to forms.LocalizedDateTimeField optional and fallback to settings.TIME_ZONE when not given.
    git-svn-id: https://django-timezones.googlecode.com/svn/trunk@7 86ebb30f-654e-0410-bc0d-7bf82786d749

--- timezones/forms.py
+++ timezones/forms.py
@@ -8,9 +8,9 @@ class LocalizedDateTimeField(forms.DateTimeField):
     """
     Converts the datetime from the user timezone to settings.TIME_ZONE.
     """
-    def __init__(self, timezone, *args, **kwargs):
-        self.tz = pytz.timezone(timezone)
+    def __init__(self, timezone=None, *args, **kwargs):
         super(LocalizedDateTimeField, self).__init__(*args, **kwargs)
+        self.tz = pytz.timezone(timezone or settings.TIME_ZONE)
         
     def clean(self, value):
         value = super(LocalizedDateTimeField, self).clean(value)
--- timezones/tests.py
+++ timezones/tests.py
@@ -6,7 +6,14 @@ __test__ = {"API_TESTS": r"""
 
 >>> from timezones import forms
 
->>> f = forms.LocalizedDateTimeField("America/Denver")
+# the default case where no timezone is given explicitly.
+# uses settings.TIME_ZONE.
+>>> f = forms.LocalizedDateTimeField()
+>>> f.clean("2008-05-30 14:30:00")
+datetime.datetime(2008, 5, 30, 14, 30, tzinfo=<UTC>)
+
+# specify a timezone explicity. this may come from a UserProfile for example.
+>>> f = forms.LocalizedDateTimeField(timezone="America/Denver")
 >>> f.clean("2008-05-30 14:30:00")
 datetime.datetime(2008, 5, 30, 20, 30, tzinfo=<UTC>)
 
