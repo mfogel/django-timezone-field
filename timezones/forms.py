663cc24fc5828f5d2c0a43f2e96528f4ecf0f7bb
Brian Rosner <brosner@gmail.com>
2008-05-30 17:02:23 +0000

    Abstracted some code out into a util function adjust_datetime_to_timezone.
    git-svn-id: https://django-timezones.googlecode.com/svn/trunk@10 86ebb30f-654e-0410-bc0d-7bf82786d749

--- timezones/forms.py
+++ timezones/forms.py
@@ -4,14 +4,16 @@ import pytz
 from django.conf import settings
 from django import newforms as forms
 
+from timezones.utils import adjust_datetime_to_timezone
+
 class LocalizedDateTimeField(forms.DateTimeField):
     """
     Converts the datetime from the user timezone to settings.TIME_ZONE.
     """
     def __init__(self, timezone=None, *args, **kwargs):
         super(LocalizedDateTimeField, self).__init__(*args, **kwargs)
-        self.tz = pytz.timezone(timezone or settings.TIME_ZONE)
+        self.timezone = timezone or settings.TIME_ZONE
         
     def clean(self, value):
         value = super(LocalizedDateTimeField, self).clean(value)
-        return self.tz.localize(value).astimezone(pytz.timezone(settings.TIME_ZONE))
+        return adjust_datetime_to_timezone(value, from_tz=self.timezone)
--- timezones/utils.py
+++ timezones/utils.py
@@ -1,6 +1,8 @@
 
 import pytz
 
+from django.conf import settings
+
 def localtime_for_timezone(value, timezone):
     """
     Given a ``datetime.datetime`` object in UTC and a timezone represented as
@@ -8,3 +10,12 @@ def localtime_for_timezone(value, timezone):
     """
     tz = pytz.timezone(str(timezone))
     return pytz.localize(value).astimezone(tz)
+
+def adjust_datetime_to_timezone(value, from_tz, to_tz=None):
+    """
+    Given a ``datetime`` object adjust it according to the from_tz timezone
+    string into the to_tz timezone string.
+    """
+    if to_tz is None:
+        to_tz = settings.TIME_ZONE
+    return pytz.timezone(from_tz).localize(value).astimezone(pytz.timezone(to_tz))
