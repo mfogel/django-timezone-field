e2740d45736cdf393785c183232b33f4f02252db
Brian Rosner <brosner@gmail.com>
2008-11-16 04:31:01 +0000

    Added a setup.py file for easy_install and the like.
    git-svn-id: https://django-timezones.googlecode.com/svn/trunk@33 86ebb30f-654e-0410-bc0d-7bf82786d749

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name = "timezones",
+    version = "0.1.0-pre",
+    author = "Brian Rosner",
+    author_email = "brosner@gmail.com",
+    url = "http://code.google.com/p/django-timezones/",
+    license = "BSD",
+    packages = [
+        "timezones",
+        "timezones.templatetags",
+    ],
+    package_dir = {"timezones": "timezones"},
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Utilities",
+        "Framework :: Django",
+    ]
+)
