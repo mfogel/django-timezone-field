b0688da4af6a6241a6784b34e81bc5c89c595147
Brian Rosner <brosner@gmail.com>
2008-12-11 17:48:26 +0000

    Removed backward compatibility of imports.
    git-svn-id: https://django-timezones.googlecode.com/svn/trunk@37 86ebb30f-654e-0410-bc0d-7bf82786d749

--- timezones/__init__.py
+++
@@ -1,3 +0,0 @@
-# for backwards compatability
-from timezones.forms import ALL_TIMEZONE_CHOICES
-TIMEZONE_CHOICES = ALL_TIMEZONE_CHOICES
--- timezones/forms.py
+++ timezones/forms.py
@@ -7,7 +7,7 @@ from django import forms
 
 from timezones.utils import adjust_datetime_to_timezone
 
-ALL_TIMEZONE_CHOICES = TIMEZONE_CHOICES = tuple(zip(pytz.all_timezones, pytz.all_timezones))
+ALL_TIMEZONE_CHOICES = tuple(zip(pytz.all_timezones, pytz.all_timezones))
 COMMON_TIMEZONE_CHOICES = tuple(zip(pytz.common_timezones, pytz.common_timezones))
 PRETTY_TIMEZONE_CHOICES = []
 for tz in pytz.common_timezones:
