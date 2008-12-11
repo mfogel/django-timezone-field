6ef7ec51fa5e5ccf4d126a2e089fa029afafcc3b
Brian Rosner <brosner@gmail.com>
2008-12-11 18:02:04 +0000

    Fixed issue #6 -- Ensure the decorator tests run when while TIME_ZONE is set to UTC. Thanks mandric for reporting this.
    git-svn-id: https://django-timezones.googlecode.com/svn/trunk@39 86ebb30f-654e-0410-bc0d-7bf82786d749

--- timezones/tests.py
+++ timezones/tests.py
@@ -1,10 +1,12 @@
 
 __test__ = {"API_TESTS": r"""
+>>> from datetime import datetime
+
 >>> from django.conf import settings
 >>> ORIGINAL_TIME_ZONE = settings.TIME_ZONE
 >>> settings.TIME_ZONE = "UTC"
 
->>> from timezones import forms
+>>> from timezones import forms, decorators
 
 # the default case where no timezone is given explicitly.
 # uses settings.TIME_ZONE.
@@ -21,11 +23,6 @@ datetime.datetime(2008, 5, 30, 20, 30, tzinfo=<UTC>)
 >>> f.clean('US/Eastern')
 <DstTzInfo 'US/Eastern' EST-1 day, 19:00:00 STD>
 
->>> settings.TIME_ZONE = ORIGINAL_TIME_ZONE
-""",
-"DECORATOR_TESTS": r"""
->>> from timezones import decorators
->>> from datetime import *
 >>> class Foo(object):
 ...     datetime = datetime(2008, 6, 20, 23, 58, 17)
 ...     @decorators.localdatetime('datetime')
@@ -42,4 +39,7 @@ datetime.datetime(2008, 6, 21, 9, 58, 17, tzinfo=<DstTzInfo 'Australia/Lindeman'
 datetime.datetime(2008, 6, 12, 13, 50, tzinfo=<UTC>)
 >>> foo.localdatetime
 datetime.datetime(2008, 6, 12, 23, 50, tzinfo=<DstTzInfo 'Australia/Lindeman' EST+10:00:00 STD>)
+
+
+>>> settings.TIME_ZONE = ORIGINAL_TIME_ZONE
 """}
