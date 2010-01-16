e0855bbb86e01905ef8a715a45bce8e762dec2ac
Brian Rosner <brosner@gmail.com>
2010-01-16 15:24:35 -0700

    modified get_version for dev builds

--- timezones/__init__.py
+++ timezones/__init__.py
@@ -1,11 +1,14 @@
 VERSION = (0, 1, 4, "final")
 
 
+
 def get_version():
-    if VERSION[3] != "final":
-        return "%s.%s.%s%s" % (VERSION[0], VERSION[1], VERSION[2], VERSION[3])
-    else:
+    if VERSION[3] == "final":
         return "%s.%s.%s" % (VERSION[0], VERSION[1], VERSION[2])
+    elif VERSION[3] == "dev":
+        return "%s.%s.%s.%s%s" % (VERSION[0], VERSION[1], VERSION[2], VERSION[3], VERSION[4])
+    else:
+        return "%s.%s.%s%s" % (VERSION[0], VERSION[1], VERSION[2], VERSION[3])
 
 
 __version__ = get_version()
