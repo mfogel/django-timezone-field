994fb48e62f73397889c244355d91fa2741d2feb
Brian Rosner <brosner@gmail.com>
2010-01-16 15:27:08 -0700

    bumped to 0.2.dev1 and fixed version generation when VERSION[2] == 0

--- timezones/__init__.py
+++ timezones/__init__.py
@@ -1,4 +1,4 @@
-VERSION = (0, 1, 4, "final")
+VERSION = (0, 2, 0, "dev", 1)
 
 
 
@@ -6,6 +6,8 @@ def get_version():
     if VERSION[3] == "final":
         return "%s.%s.%s" % (VERSION[0], VERSION[1], VERSION[2])
     elif VERSION[3] == "dev":
+        if VERSION[2] == 0:
+            return "%s.%s.%s%s" % (VERSION[0], VERSION[1], VERSION[3], VERSION[4])
         return "%s.%s.%s.%s%s" % (VERSION[0], VERSION[1], VERSION[2], VERSION[3], VERSION[4])
     else:
         return "%s.%s.%s%s" % (VERSION[0], VERSION[1], VERSION[2], VERSION[3])
