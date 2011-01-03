12172585a164d046411751931c0ecf43c4b78148
Brian Rosner <brosner@gmail.com>
2011-01-02 17:18:39 -0700

    Added support for multi-db in LocalizedDateTimeField.

--- timezones/fields.py
+++ timezones/fields.py
@@ -83,7 +83,7 @@ class LocalizedDateTimeField(models.DateTimeField):
         defaults.update(kwargs)
         return super(LocalizedDateTimeField, self).formfield(**defaults)
     
-    def get_db_prep_save(self, value):
+    def get_db_prep_save(self, value, connection=None):
         """
         Returns field's value prepared for saving into a database.
         """
@@ -93,9 +93,9 @@ class LocalizedDateTimeField(models.DateTimeField):
                 value = default_tz.localize(value)
             else:
                 value = value.astimezone(default_tz)
-        return super(LocalizedDateTimeField, self).get_db_prep_save(value)
+        return super(LocalizedDateTimeField, self).get_db_prep_save(value, connection=connection)
     
-    def get_db_prep_lookup(self, lookup_type, value):
+    def get_db_prep_lookup(self, lookup_type, value, connection=None, prepared=None):
         """
         Returns field's value prepared for database lookup.
         """
@@ -104,7 +104,7 @@ class LocalizedDateTimeField(models.DateTimeField):
             value = default_tz.localize(value)
         else:
             value = value.astimezone(default_tz)
-        return super(LocalizedDateTimeField, self).get_db_prep_lookup(lookup_type, value)
+        return super(LocalizedDateTimeField, self).get_db_prep_lookup(lookup_type, value, connection=connection, prepared=prepared)
 
 
 def prep_localized_datetime(sender, **kwargs):
