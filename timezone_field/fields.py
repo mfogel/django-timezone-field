17014601a170967c5ecbbd289551a4db0783df6b
Mike Fogel <mike@fogel.ca>
2012-04-12 18:17:24 -0700

    Removing use of coerce_timezone
    Letting exceptions propogate without modification.

--- timezone_field/fields.py
+++ timezone_field/fields.py
@@ -1,9 +1,10 @@
+import pytz
+
 from django.conf import settings
 from django.db import models
 from django.utils.encoding import smart_unicode, smart_str
 
 from timezone_field import zones
-from timezone_field.utils import coerce_timezone_value
 
 
 class TimeZoneField(models.CharField):
@@ -29,9 +30,8 @@ class TimeZoneField(models.CharField):
 
     def to_python(self, value):
         value = super(TimeZoneField, self).to_python(value)
-        if value is None:
-            return None # null=True
-        return coerce_timezone_value(value)
+        # if db is corrupted, will throw a pytz.UnknownTimeZoneError
+        return pytz.timezone(value)
 
     def get_prep_value(self, value):
         if value is not None:
--- timezone_field/forms.py
+++ timezone_field/forms.py
@@ -1,12 +1,13 @@
+import pytz
+
 from django import forms
 
 from timezone_field import zones
-from timezone_field.utils import coerce_timezone_value
 
 
 class TimeZoneField(forms.TypedChoiceField):
     def __init__(self, *args, **kwargs):
         if not "choices" in kwargs:
             kwargs["choices"] = zones.PRETTY_TIMEZONE_CHOICES
-        kwargs["coerce"] = coerce_timezone_value
+        kwargs["coerce"] = pytz.timezone
         super(TimeZoneField, self).__init__(*args, **kwargs)
