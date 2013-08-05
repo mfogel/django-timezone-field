e5061a1d6b8ab7f6a1fe561c028e3d2b3d6ef64c
Mike Fogel <mike@fogel.ca>
2013-08-04 18:00:32 -0700

    Fix coersion on django 1.6

--- timezone_field/forms.py
+++ timezone_field/forms.py
@@ -1,11 +1,19 @@
 import pytz
+from django.core.exceptions import ValidationError
 from django import forms
 
 
 class TimeZoneFormField(forms.TypedChoiceField):
     def __init__(self, *args, **kwargs):
+
+        def coerce_to_pytz(val):
+            try:
+                return pytz.timezone(val)
+            except pytz.UnknownTimeZoneError:
+                raise ValidationError("Unknown time zone: '%s'" % val)
+
         defaults = {
-            'coerce': lambda val: pytz.timezone(val),
+            'coerce': coerce_to_pytz,
             'choices': [(tz, tz) for tz in pytz.all_timezones],
             'empty_value': None,
         }
