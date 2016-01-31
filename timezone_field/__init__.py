32e9cb51bd1567778062692b7da3afd429f53e90
Mike Fogel <mike@fogel.ca>
2016-01-30 16:08:48 -0800

    Pass flake8 tests

--- timezone_field/__init__.py
+++ timezone_field/__init__.py
@@ -1,5 +1,5 @@
-__version__ = '1.3'
-__all__ = ['TimeZoneField', 'TimeZoneFormField']
-
 from timezone_field.fields import TimeZoneField
 from timezone_field.forms import TimeZoneFormField
+
+__version__ = '1.3'
+__all__ = ['TimeZoneField', 'TimeZoneFormField']
