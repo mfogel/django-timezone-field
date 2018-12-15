5bcbbd5f3d3f534a51a7b88f0f763ffcfc77cd5f
SM Mashuque <smjim1998@gmail.com>
2018-12-14 19:42:49 -0500

    issue_46_implement_GMT_offset_in_timezones

--- timezone_field/fields.py
+++ timezone_field/fields.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta, time
 import pytz
 
 from django.core.exceptions import ValidationError
@@ -51,6 +52,7 @@ class TimeZoneField(models.Field):
 
         kwargs.setdefault('choices', self.CHOICES)
         kwargs.setdefault('max_length', self.MAX_LENGTH)
+        kwargs.setdefault('display_GMT_offset', False)
 
         # Choices can be specified in two forms: either
         # [<pytz.timezone>, <str>] or [<str>, <str>]
@@ -66,6 +68,10 @@ class TimeZoneField(models.Field):
         choices = kwargs['choices']
         if isinstance(choices[0][0], (six.string_types, six.binary_type)):
             kwargs['choices'] = [(pytz.timezone(n1), n2) for n1, n2 in choices]
+
+        if kwargs['display_GMT_offset']:
+            kwargs['choices'] = self._add_gmt_offset_to_choices(kwargs['choices'])
+        kwargs.pop('display_GMT_offset', None)
 
         super(TimeZoneField, self).__init__(*args, **kwargs)
 
@@ -120,3 +126,37 @@ class TimeZoneField(models.Field):
         except pytz.UnknownTimeZoneError:
             pass
         raise ValidationError("Invalid timezone '%s'" % value)
+
+    def _add_gmt_offset_to_choices(self, timezone_set):
+        """
+        Currently timezone choices items show up like this:
+        'America/New_York'
+
+        But this function formats the choices to display in this format:
+        GMT-05:00 America/New_York
+
+        :return:
+        A list of tuples in this format:
+        (<pytz.timezone>, <str>)
+        """
+        # timezones = pytz.common_timezones_set
+        gmt_timezone = pytz.timezone('Greenwich')
+        time_ref = datetime(2000, 1, 1)
+        time_zero = gmt_timezone.localize(time_ref)
+        _choices = []
+        for tz, tz_str in timezone_set:
+            # z = pytz.timezone(tz)
+            delta = (time_zero - tz.localize(time_ref)).total_seconds()
+            h = (datetime.min + timedelta(seconds=delta.__abs__())).hour
+            gmt_diff = time(h).strftime('%H:%M')
+            pair_one = tz
+            pair_two = "GMT{sign}{gmt_diff} {timezone}".format(
+                sign="-" if delta < 0 else "+",
+                gmt_diff=gmt_diff,
+                timezone=tz
+            )
+            _choices.append((delta, pair_one, pair_two))
+
+        _choices.sort(key=lambda x: x[0])
+        choices = [(one, two) for zero, one, two in _choices]
+        return choices
