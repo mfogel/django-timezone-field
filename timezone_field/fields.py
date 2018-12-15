81b9e1e17da6adc1d520661fe74a488e68efc86a
SM Mashuque <smjim1998@gmail.com>
2018-12-14 20:49:02 -0500

    write tests and move function to utils. Probably makes more sense.

--- timezone_field/fields.py
+++ timezone_field/fields.py
@@ -1,4 +1,3 @@
-from datetime import datetime, timedelta, time
 import pytz
 
 from django.core.exceptions import ValidationError
@@ -6,7 +5,7 @@ from django.db import models
 from django.utils import six
 from django.utils.encoding import force_text
 
-from timezone_field.utils import is_pytz_instance
+from timezone_field.utils import is_pytz_instance, add_gmt_offset_to_choices
 
 
 class TimeZoneField(models.Field):
@@ -70,7 +69,7 @@ class TimeZoneField(models.Field):
             kwargs['choices'] = [(pytz.timezone(n1), n2) for n1, n2 in choices]
 
         if kwargs['display_GMT_offset']:
-            kwargs['choices'] = self._add_gmt_offset_to_choices(kwargs['choices'])
+            kwargs['choices'] = add_gmt_offset_to_choices(kwargs['choices'])
         kwargs.pop('display_GMT_offset', None)
 
         super(TimeZoneField, self).__init__(*args, **kwargs)
@@ -126,36 +125,3 @@ class TimeZoneField(models.Field):
         except pytz.UnknownTimeZoneError:
             pass
         raise ValidationError("Invalid timezone '%s'" % value)
-
-    def _add_gmt_offset_to_choices(self, timezone_set):
-        """
-        Currently timezone choices items show up like this:
-        'America/New_York'
-
-        But this function formats the choices to display in this format:
-        GMT-05:00 America/New_York
-
-        :return:
-        A list of tuples in this format:
-        (<pytz.timezone>, <str>)
-        """
-        gmt_timezone = pytz.timezone('Greenwich')
-        time_ref = datetime(2000, 1, 1)
-        time_zero = gmt_timezone.localize(time_ref)
-        _choices = []
-        for tz, tz_str in timezone_set:
-            # z = pytz.timezone(tz)
-            delta = (time_zero - tz.localize(time_ref)).total_seconds()
-            h = (datetime.min + timedelta(seconds=delta.__abs__())).hour
-            gmt_diff = time(h).strftime('%H:%M')
-            pair_one = tz
-            pair_two = "GMT{sign}{gmt_diff} {timezone}".format(
-                sign="-" if delta < 0 else "+",
-                gmt_diff=gmt_diff,
-                timezone=tz_str.replace('_', ' ')
-            )
-            _choices.append((delta, pair_one, pair_two))
-
-        _choices.sort(key=lambda x: x[0])
-        choices = [(one, two) for zero, one, two in _choices]
-        return choices
