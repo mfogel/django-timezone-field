110b61f99f9129539caa0d7724036298813f5f55
tomturner <tom@twt.me.uk>
2020-08-11 16:07:27 +0100

    A fix #63 for the display_GMT_offset

--- timezone_field/forms.py
+++ timezone_field/forms.py
@@ -1,23 +1,40 @@
+import datetime
+
 import pytz
 from django.core.exceptions import ValidationError
 from django import forms
 
 
 class TimeZoneFormField(forms.TypedChoiceField):
-    def __init__(self, *args, **kwargs):
 
-        def coerce_to_pytz(val):
-            try:
-                return pytz.timezone(val)
-            except pytz.UnknownTimeZoneError:
-                raise ValidationError("Unknown time zone: '%s'" % val)
+    @staticmethod
+    def coerce_to_pytz(val):
+        try:
+            return pytz.timezone(val)
+        except pytz.UnknownTimeZoneError:
+            raise ValidationError("Unknown time zone: '%s'" % val)
+
+    def __init__(self, *args, **kwargs):
+        if kwargs.get('display_GMT_offset', False):
+            choices = []
+            dt = datetime.datetime.utcnow()
+            for tz in pytz.common_timezones:
+                a = self.coerce_to_pytz(tz)
+                offset = a.utcoffset(dt)
+                offset_days = offset.days
+                offset_hours = offset_days * 24 + offset.seconds / 3600.0
+                name = "{:s} (UTC {:d}:{:02d})".format(tz.replace('_', ' '),
+                                                       int(offset_hours),
+                                                       int((offset_hours % 1) * 60))
+                choices.append((tz, name))
+        else:
+            choices = [(tz, tz.replace('_', ' ')) for tz in pytz.common_timezones]
 
         defaults = {
-            'coerce': coerce_to_pytz,
-            'choices': [
-                (tz, tz.replace('_', ' ')) for tz in pytz.common_timezones
-            ],
+            'coerce': self.coerce_to_pytz,
+            'choices': choices,
             'empty_value': None,
         }
+        del kwargs['display_GMT_offset']
         defaults.update(kwargs)
         super(TimeZoneFormField, self).__init__(*args, **defaults)
