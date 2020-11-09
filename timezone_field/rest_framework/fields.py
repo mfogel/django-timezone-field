c87d38427f5c07b92d79d1bbc442c50aef8c2c2e
starryrbs <1322096624@qq.com>
2020-11-09 17:59:41 +0800

    Add django rest framework support

---
+++ timezone_field/rest_framework/fields.py
@@ -0,0 +1,19 @@
+import pytz
+from rest_framework.fields import Field
+from django.utils.translation import gettext_lazy as _
+from django.utils.encoding import force_str
+
+
+class TimeZoneField(Field):
+    default_error_messages = {
+        'invalid': _('A valid timezone is required.'),
+    }
+
+    def to_internal_value(self, data):
+        try:
+            return pytz.timezone(force_str(data))
+        except pytz.UnknownTimeZoneError:
+            self.fail('invalid')
+
+    def to_representation(self, value):
+        return str(value)
