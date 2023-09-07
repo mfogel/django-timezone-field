96463be65f2a4d70f0517be66110818dbb80cf68
Mike Fogel <mike@fogel.ca>
2023-09-06 21:44:49 -0700

    Add some more get_tz_backend tests

--- tests/test_backends.py
+++ tests/test_backends.py
@@ -1,6 +1,8 @@
 from django import VERSION
 
 from timezone_field.backends import USE_PYTZ_DEFAULT, get_tz_backend
+from timezone_field.backends.pytz import PYTZBackend
+from timezone_field.backends.zoneinfo import ZoneInfoBackend
 
 
 def test_use_pytz_default_USE_DEPRECATED_PYTZ_unset():
@@ -9,3 +11,11 @@ def test_use_pytz_default_USE_DEPRECATED_PYTZ_unset():
 
 def test_get_tz_backend_when_use_pytz_is_none():
     assert get_tz_backend(None) is get_tz_backend(USE_PYTZ_DEFAULT)
+
+
+def test_get_tz_backend_when_use_pytz_is_true():
+    assert isinstance(get_tz_backend(True), PYTZBackend)
+
+
+def test_get_tz_backend_when_use_pytz_is_false():
+    assert isinstance(get_tz_backend(False), ZoneInfoBackend)
