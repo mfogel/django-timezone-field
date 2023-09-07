16639cf5ab636e5c13c1a18bbeb8c6faaa3c4a45
Mike Fogel <mike@fogel.ca>
2023-09-06 21:15:46 -0700

    Replicate failing behavior in #109

--- tests/test_backends.py
+++ tests/test_backends.py
@@ -1,7 +1,11 @@
 from django import VERSION
 
-from timezone_field.backends import USE_PYTZ_DEFAULT
+from timezone_field.backends import USE_PYTZ_DEFAULT, get_tz_backend
 
 
 def test_use_pytz_default_USE_DEPRECATED_PYTZ_unset():
     assert USE_PYTZ_DEFAULT is (VERSION < (4, 0))
+
+
+def test_get_tz_backend_when_use_pytz_is_none():
+    assert get_tz_backend(use_pytz=None) is USE_PYTZ_DEFAULT
