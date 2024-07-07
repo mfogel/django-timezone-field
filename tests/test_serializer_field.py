dbe227d9adafb1605487869c57a8e756c51e9852
Mike Fogel <mike@fogel.ca>
2024-07-06 17:16:04 -0700

    Add tests to clarify allow_null behavior

--- tests/test_serializer_field.py
+++ tests/test_serializer_field.py
@@ -12,6 +12,14 @@ def TimeZoneSerializer(use_pytz):
 
     yield _TimeZoneSerializer
 
+@pytest.fixture
+def TimeZoneSerializerAllowNull(use_pytz):
+    class _TimeZoneSerializer(serializers.Serializer):
+        # pylint: disable=abstract-method
+        tz = TimeZoneSerializerField(use_pytz=use_pytz, allow_null=True)
+
+    yield _TimeZoneSerializer
+
 
 def test_invalid_str(TimeZoneSerializer, invalid_tz):
     serializer = TimeZoneSerializer(data={"tz": invalid_tz})
@@ -41,3 +49,14 @@ def test_valid_with_timezone_object(TimeZoneSerializer, pst, pst_tz):
     assert serializer.is_valid()
     assert serializer.data["tz"] == pst
     assert serializer.validated_data["tz"] == pst_tz
+
+
+def test_allow_null(TimeZoneSerializer, TimeZoneSerializerAllowNull):
+    serializer_disallow_null = TimeZoneSerializer(data={"tz": None})
+    serializer_allow_null = TimeZoneSerializerAllowNull(data={"tz": None})
+    assert serializer_disallow_null.is_valid() is False
+    assert serializer_disallow_null.data == {"tz": None}
+    assert serializer_disallow_null.validated_data == {}
+    assert serializer_allow_null.is_valid()
+    assert serializer_allow_null.data == {"tz": None}
+    assert serializer_allow_null.validated_data == {"tz": None}
